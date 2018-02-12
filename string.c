d12a4032ccb41dd2a675f4f9478daa040619a3a1
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-12 16:52:53 +0530

    c pgm
    good

---
+++ string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int string[];
+char n;
+printf("\n Enter the string:");
+scanf("%s",&n);
+if(n>=65&&n<=90||n>=97&&n<=122)
+{
+printf("no);
+}
+else
+{
+printf("yes");
+}
+}
+
